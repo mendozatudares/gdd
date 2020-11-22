f585d1a251b6dbc1c66dd3e336e12602b76a340b
Kevin Mendoza Tudares <kevinmendozatudares2022@u.northwestern.edu>
2020-11-22 17:36:58 -0600

    implement Player firing (basic)

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -5,17 +5,25 @@
 
 public class Player : MonoBehaviour
 {
+    public Object BulletPrefab;
     public float JumpForce = 5.0f;
     public float Speed = 1.0f;
+    public float FireTime = 0.5f;
+    public AudioClip FireAudioClip;
+    public AudioClip DeathAudioClip;
     private Vector3 _jump;
     private bool _isGrounded;
+    private float _lastFire;
     private Rigidbody _rb;
+    private AudioSource _as;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _jump = new Vector3(0, JumpForce, 0);
+        _as = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -49,6 +57,30 @@ public class Player : MonoBehaviour
         var dir = mousePos - playerPos;
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+
+        // Fire if the left mouse button is clicked and enough time has passed
+        if (!Input.GetKeyDown(KeyCode.Mouse0) || (Time.time - _lastFire) < FireTime) return;
+        _lastFire = Time.time;
+        Fire();
+    }
+
+    private void Fire()
+    {
+        Debug.Log("Fire!");
+        // _as.PlayOneShot(FireAudioClip, 0.7f);
+        // Make a new bullet in front of player
+        // Instantiate(BulletPrefab,
+        //     transform.TransformPoint(new Vector3(1f, 0, 0)),
+        //     transform.rotation);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name.Contains("Enemy"))
+        {
+            // _as.PlayOneShot(DeathAudioClip, 0.7f);
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionStay()
