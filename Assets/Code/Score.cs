d4c9138233b0a8750e09951b35ad8d4a602aaa0e
akwasiboachie <akwasiboachie2021@u.northwestern.edu>
2020-11-29 18:54:03 -0500

    Bullet and Score

---
+++ Assets/Code/Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private static int score;
+    private static Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        score = 0;
+        UpdateText();
+    }
+
+    public static void UpdateScore()
+    {
+        score += 1;
+        UpdateText();
+    }
+
+    private static void UpdateText()
+    {
+        scoreText.text = string.Format("Score: {0}", score);
+    }
+}
