34eeaeb537904bd25e287e987c6c185b08020d97
Kevin Mendoza Tudares <kevinmendozatudares2022@u.northwestern.edu>
2020-12-03 23:10:21 -0500

    play score sound

--- Assets/Code/Score.cs
+++ Assets/Code/Score.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    public static AudioClip ScoreAudioClip;
+    private static AudioSource _audioSource;
     private static int _score;
     private static Text _scoreText;
     private static int _goal;
@@ -11,6 +13,8 @@ public class Score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        ScoreAudioClip = Resources.Load<AudioClip>("Sounds/score");
+        _audioSource = GetComponent<AudioSource>();
         _scoreText = GetComponent<Text>();
         _score = 0;
         _goal = Camera.main.GetComponent<LevelManager>().enemyNumber;
@@ -20,6 +24,7 @@ public class Score : MonoBehaviour
     public static void UpdateScore()
     {
         _score += 1;
+        _audioSource.PlayOneShot(ScoreAudioClip);
         UpdateText();
         if (_score >= _goal)
             Camera.main.GetComponent<LevelManager>().CallNextLevel();
